e9e4bd696cbdab171780947b2c9b2792f816bb72
Fernando <fernando.tomita@fatec.sp.gov.br>
2022-11-20 21:14:47 -0300

    Formatacao da data

--- app/src/main/java/com/marcacorrida/historico/CorridaAdapter.java
+++ app/src/main/java/com/marcacorrida/historico/CorridaAdapter.java
@@ -3,6 +3,7 @@ package com.marcacorrida.historico;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,9 +16,9 @@ import java.util.Locale;
 import com.marcacorrida.R;
 import com.marcacorrida.model.Corrida;
 
-public class CorridaAdapter extends RecyclerView.Adapter<CorridaViewHolder> {
+public class CorridaAdapter extends RecyclerView.Adapter<CorridaAdapter.CorridaViewHolder> {
     private List<Corrida> dataSet;
-    private DateFormat DDMMYY_FORMATER = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()); //USING API LEVEL 23
+    private DateFormat DDMMYY_FORMATER = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault());
 
     public CorridaAdapter(List<Corrida> dataSet) {
         this.dataSet = dataSet;
@@ -47,6 +48,39 @@ public class CorridaAdapter extends RecyclerView.Adapter<CorridaViewHolder> {
     public int getItemCount() {
         return dataSet.size() + 1;
     }
+
+    public class CorridaViewHolder extends RecyclerView.ViewHolder {
+        private TextView txtVwNome;
+        private TextView txtVwDuracao;
+        private TextView txtVwPassos;
+        private TextView txtVwData;
+
+        public CorridaViewHolder(View view) {
+            super(view);
+
+            txtVwNome = view.findViewById(R.id.txtVwNome);
+            txtVwDuracao = view.findViewById(R.id.txtVwDuracao);
+            txtVwPassos = view.findViewById(R.id.txtVwPassos);
+            txtVwData = view.findViewById(R.id.txtVwData);
+        }
+
+        public TextView getTxtVwNome() {
+            return txtVwNome;
+        }
+
+        public TextView getTxtVwDuracao() {
+            return txtVwDuracao;
+        }
+
+        public TextView getTxtVwPassos() {
+            return txtVwPassos;
+        }
+
+        public TextView getTxtVwData() {
+            return txtVwData;
+        }
+    }
+
 }
 
 
